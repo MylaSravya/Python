290b353825f89cf828489180c99f165d04705056
sravyamyla <sm465@njit.edu>
2023-01-17 18:26:30 -0500

    updated code

---
+++ python_practice/001-python-core-language/code/007_compositions.py
@@ -0,0 +1,16 @@
+---
+YamlDesc: CONTENT-ARTICLE
+Title: python convert dictionary to list, conver dictionary to tuple
+MetaDescription: python read nested list dictionary tuple convert dictionary to list to tuple example code, tutorials
+MetaKeywords: python convert dictionary to list, conver dictionary to tuple example code, tutorials
+Author: Sravya Myla
+ContentName: composition
+---
+
+# PYTHON Convert Tuple to List to Dictionary
+```python
+
+# convert 2 Lists to Dict
+print(dict(zip([x for x in 'abcdefg'], xrange(1, 8))))
+
+```
